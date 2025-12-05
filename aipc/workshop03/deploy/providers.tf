ce47b97878ae8101587baaefd7bafc8dc83e388e
Patrick Zhang <patrickzhangpu@gmail.com>
2025-12-05 08:32:51 +0000

    workshop 03 done!

--- aipc/workshop03/deploy/providers.tf
+++ aipc/workshop03/deploy/providers.tf
@@ -8,18 +8,6 @@ terraform {
     }
   }
 
-  backend "s3" {
-    endpoints = {
-      s3 = "https://sgp1.digitaloceanspaces.com"
-    }
-
-    skip_credentials_validation  = true
-    skip_metadata_api_check      = true
-    skip_region_validation       = true
-    skip_requesting_account_id   = true
-    skip_s3_checksum             = true
-  
-}
 }
 
 provider "digitalocean" {
--- aipc/workshop03/deploy/resources.tf
+++ aipc/workshop03/deploy/resources.tf
@@ -3,18 +3,34 @@ data "digitalocean_ssh_key" "mykey" {
 }
 
 data "digitalocean_image" "nginx_image" {
-    name   = "aipc-day3-nginx-1764902423"
+    name   = "aipc-day3-code-server-1764922344"
 }
 
-resource "digitalocean_droplet" "day3_mynginx" {
-  name = "day3-mynginx"
+resource "digitalocean_droplet" "day3_codeserver_nginx" {
+  name = "day3-codeserver-nginx"
   image = data.digitalocean_image.nginx_image.id
   region = "sgp1"
   size = var.DO_SIZE
   tags = ["aipc", "day3", "nginx"]
   ssh_keys = [data.digitalocean_ssh_key.mykey.id]
+
+
+     connection {
+     type = "ssh"
+     user = "root"
+     private_key = file("~/.ssh/id_ed25519")
+     host = self.ipv4_address
+   }
+
+   provisioner "remote-exec" {
+      inline = [  
+         "sudo sed -i 's/server_name[[:space:]]*_;/server_name ${self.ipv4_address};/' /etc/nginx/sites-available/code-server.conf",
+         "systemctl daemon-reload",
+         "systemctl restart nginx"
+      ]
+   }
 }
 
 output "mynginx_ip" {
-  value = digitalocean_droplet.day3_mynginx.ipv4_address    
+  value = digitalocean_droplet.day3_codeserver_nginx.ipv4_address    
 }
